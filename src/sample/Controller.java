9e31b38b4319fef6b29bea5328d36c0e287aa308
Dmitry <f_o_r_w_a_r_d@inbox.ru>
2020-06-24 23:28:42 +0500

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,11 @@
+package sample;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    public Button btnSend;
+    public TextArea taConversation;
+    public TextField tfMessageToSend;
+}
